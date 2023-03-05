9828716abca02c0c214424d78b2e2a3628451236
yuiseki <yuiseki@gmail.com>
2023-03-05 11:57:59 +0900

    fix docker install step

--- stage100/01-install-docker/00-run.sh
+++ stage100/01-install-docker/00-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" DEBIAN_FRONTEND=noninteractive apt-get update -qq > /dev/null
+	SUDO_USER="${FIRST_USER_NAME}" DEBIAN_FRONTEND=noninteractive apt-get install -y -qq apt-transport-https ca-certificates curl >/dev/null
 	SUDO_USER="${FIRST_USER_NAME}" command docker > /dev/null || curl -sSL https://get.docker.com | sh
 	SUDO_USER="${FIRST_USER_NAME}" usermod -aG docker ${FIRST_USER_NAME}
 	SUDO_USER="${FIRST_USER_NAME}" systemctl enable docker.service
