37dc6c0ca0325a41c285fee2bf86bcb9b045eda0
yuiseki <yuiseki@gmail.com>
2023-03-10 20:22:34 +0900

    chmod 777 -R /usr/local/bin

--- stage101/20-install-tippecanoe/01-run-chroot.sh
+++ stage101/20-install-tippecanoe/01-run-chroot.sh
@@ -2,12 +2,14 @@
 
 mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
 
-if [ ! -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/tippecanoe ]; then
-  cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
-  git clone --depth 1 https://github.com/felt/tippecanoe.git
-  cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/tippecanoe
-  make -j LDFLAGS="-latomic"; make install
+if type "tippecanoe" > /dev/null 2>&1; then
+  echo "tippecanoe exist!"
+  exit 0
 fi
 
-chmod 777 -R ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
-chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} -R ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
+cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
+git clone --depth 1 https://github.com/felt/tippecanoe.git
+cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/tippecanoe
+make -j LDFLAGS="-latomic"
+make install
+chmod 777 -R /usr/local/bin
--- stage101/22-install-tilemaker/01-run-chroot.sh
+++ stage101/22-install-tilemaker/01-run-chroot.sh
@@ -2,12 +2,15 @@
 
 mkdir -p ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
 
-if [ ! -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/tilemaker ]; then
-  cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
-  git clone --depth 1 https://github.com/systemed/tilemaker.git
-  cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/tilemaker
-  make -j LDFLAGS="-latomic"; make install
+if type "tilemaker" > /dev/null 2>&1; then
+  echo "tilemaker exist!"
+  exit 0
 fi
 
-chmod 777 -R ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
-chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} -R ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/
+cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
+
+git clone --depth 1 https://github.com/systemed/tilemaker.git
+cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/tilemaker
+make -j LDFLAGS="-latomic"
+make install
+chmod 777 -R /usr/local/bin
