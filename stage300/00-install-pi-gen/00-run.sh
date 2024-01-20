57faf5dfcc568aea72181b5515b88e9f766c0b4a
yuiseki <yuiseki@gmail.com>
2024-01-20 18:21:52 +0900

    chmod, chownが必要

--- stage300/00-install-pi-gen/00-run.sh
+++ stage300/00-install-pi-gen/00-run.sh
@@ -6,3 +6,8 @@ if [ ! -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src/unvt-pi-gen ]; then
   cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/src
   git clone --depth 1 https://github.com/yuiseki/unvt-pi-gen.git
 fi
+
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" chmod 755 -R /home/${FIRST_USER_NAME}/src
+	SUDO_USER="${FIRST_USER_NAME}" chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} -R /home/${FIRST_USER_NAME}/src
+EOF
